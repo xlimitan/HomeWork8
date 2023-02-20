432387f4f0ff483fb24ff1e2e7a55f12862211d8
xlimitan <xlimitan@yandex.ru>
2023-02-20 15:20:46 +0300

    Задание № 1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,23 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-        // Пишем код для задачи 1
+        int [] one = new int [3];
+        one [0] = 1;
+        one [1] = 2;
+        one [2] = 3;
+        for (int i = 0; i < one.length; i++) {
+            System.out.println(one[i]);
+        }
+        System.out.println();
+        double [] two = {1.57, 7.654, 9.986};
+        for (int i = 0; i < two.length; i++) {
+            System.out.println(two[i]);
+        }
+        System.out.println();
+        int [] three = {37, 7, 9, 82, 15};
+        for (int i = 0; i < three.length; i++) {
+            System.out.println(three[i]);
+        }
     }
 
     public static void task2 () {
