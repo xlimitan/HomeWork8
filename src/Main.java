fb16402d605bdfa71c113c9c4aa0aa8424455e62
xlimitan <xlimitan@yandex.ru>
2023-02-20 17:06:36 +0300

    Задание № 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
     }
 
     public static void task1() {
-        System.out.println("Задача 1");
+        System.out.println("Задача 1-2");
         int[] one = new int[3];
         one[0] = 1;
         one[1] = 2;
         one[2] = 3;
         double[] two = {1.57, 7.654, 9.986};
-        int[] tree = {'a', 'b', 'c'};
+        char[] three = {'a', 'b', 'c'};
 
         for (int i = 0; i < one.length; i++) {
             System.out.print(one[i]);
@@ -26,12 +28,47 @@ public class Main {
             }
         }
         System.out.println();
-        for (int i = 0; i < tree.length; i++) {
-            System.out.print(tree[i]);
-            if (i != tree.length - 1) {
+        for (int i = 0; i < three.length; i++) {
+            System.out.print(three[i]);
+            if (i != three.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
+    }
+    public static void task2() {
+        System.out.println("Задача 3");
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        double[] two = {1.57, 7.654, 9.986};
+        char[] three = {'a', 'b', 'c'};
+
+        for (int i  = one.length - 1; i >= 0; i--) {
+            System.out.print(one[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i  = two.length - 1; i >= 0; i--) {
+            System.out.print(two[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i  = three.length - 1; i >= 0; i--) {
+            System.out.print(three[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+    public static void task3() {
+        System.out.println("Задача 4");
+
     }
 }
