fb78e260969c90abf0e683cd4d59e4104177c637
xlimitan <xlimitan@yandex.ru>
2023-02-20 16:48:54 +0300

    Задание № 2

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,29 @@ public class Main {
         one[0] = 1;
         one[1] = 2;
         one[2] = 3;
-        double [] two = {1.57, 7.654, 9.986};
-        int [] tree = {'a', 'b', 'c'};
+        double[] two = {1.57, 7.654, 9.986};
+        int[] tree = {'a', 'b', 'c'};
+
+        for (int i = 0; i < one.length; i++) {
+            System.out.print(one[i]);
+            if (i != one.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < two.length; i++) {
+            System.out.print(two[i]);
+            if (i != two.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < tree.length; i++) {
+            System.out.print(tree[i]);
+            if (i != tree.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
